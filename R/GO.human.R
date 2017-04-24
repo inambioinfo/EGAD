db957ef94aed99928d93bf78701718d4fc5e6add
hpages@fhcrc.org <hpages@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-04-24 19:45:44 +0000

    Creating branch for BioC 3.5 release
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_5/madman/Rpacks/EGAD@129128 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/GO.human.R
@@ -0,0 +1,22 @@
+#' @name GO.human
+#' @title GO - human
+#' 
+#' @description
+#' A dataset of the gene GO associations 
+#'
+#'
+#' @docType data
+#' 
+#' @format A data frame with 2511938 rows and 4 variables:
+#' \describe{
+#'  \item{name}{gene symbol}
+#'  \item{entrezID}{entrez identifier}
+#'  \item{GO}{gene ontology term ID}
+#'  \item{evidence}{evidence code}
+#'  
+#' }
+#'  @source \url{http://geneontology.org/}
+#'  
+#' @rdname GO.human
+#'
+NULL
---
+++ R/GO.mouse.R
@@ -0,0 +1,21 @@
+#' @name GO.mouse
+#' @title GO - mouse
+#'
+#' @description
+#' A dataset of the gene GO associations 
+#'
+#' @docType data
+#'
+#' @format A data frame with 2086086 rows and 4 variables:
+#' \describe{
+#'  \item{name}{gene symbol}
+#'  \item{entrezID}{entrez identifier}
+#'  \item{GO}{gene ontology term ID}
+#'  \item{evidence}{evidence code}
+#'  
+#' }
+#'  @source \url{http://geneontology.org/}
+#'    
+#' @rdname GO.mouse
+#'
+NULL
